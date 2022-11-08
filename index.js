2f24ae153c5f934b73d7e05e9aa15d322e68346b
siamshahriar <siamshahriar99@gmail.com>
2022-11-08 18:39:24 +0600

    mongodb config added

--- index.js
+++ index.js
@@ -11,14 +11,27 @@ app.use(express.json());
 
 // mongoDB section ================
 
+//config
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.dhw1j4v.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dhw1j4v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+//config
+
+//paths
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
+
+//paths
 
 // mongoDB section ================
 
