88242589f3d18216a1760035d3d556fa87ed652b
siamshahriar <siamshahriar99@gmail.com>
2022-11-09 11:00:16 +0600

    server where user can delete his reviews

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       const result = await reviews.updateOne(query, updatedDoc);
       res.send(result);
     });
+
+    //it will delete the review based on review id
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviews.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
